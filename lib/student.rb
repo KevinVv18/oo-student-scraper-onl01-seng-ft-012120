5f31e20537cbcdd410249ad808a19cd494b4544d
Kevin Villalobos <kevinv15v@gmail.com>
2020-02-24 03:42:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,10 +5,16 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    self.send("name=", student_hash[:name])
+    self.send("location=", student_hash[:location])
+    self.send("profile_url=", student_hash[:profile_url])
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
+    
+    students_array.each do |student_hash|
+      Student.new(student_hash)
     
   end
 
